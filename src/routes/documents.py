02c359d80654390708ba61f48b9b093309bcaf5c
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-08-18 20:33:25 -0300

    Refactor documents route

--- src/routes/documents.py
+++ src/routes/documents.py
@@ -2,8 +2,8 @@ import os
 import logging
 from flask import Blueprint, request, jsonify
 from src.chunks.chunks import Chunker
-from src.utils.extract_doc_content import extract_doc_content
 from src.chromadb.client import ChromaDBClient
+from src.documents.document_processor import DocumentProcessor
 
 documents_bp = Blueprint("documents", __name__)
 
@@ -25,21 +25,28 @@ def ingest_document():
 
     try:
         file_bytes = file.stream.read()
-        doc_content = extract_doc_content(file_bytes=file_bytes, extension=extension)
+
+        document_processor = DocumentProcessor(local_storage_path=os.getenv("BUCKET_DIR", "bucket"), img_scale=2.0)
+        text_content, images = document_processor.run(file_bytes=file_bytes, file_name=file.filename)
 
         chunker = Chunker(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
-        chunks = chunker.run(doc_content=doc_content)
+        content_chunks = chunker.run(doc_content=text_content)
 
         client = ChromaDBClient(
             host=os.getenv("CHROMA_HOST", "localhost"),
             port=int(os.getenv("CHROMA_PORT", 8000))
         )
 
-        collection_name = "documents"
-        client.ingest_chunks(chunks=chunks, collection_name=collection_name)
+        # Save chunks to ChromaDB
+        client.ingest_chunks(chunks=content_chunks, collection_name="documents")
+        logging.info(f"Successfully added {len(content_chunks)} document chunks to 'documents' collection")
 
-        return jsonify({"message": f"Successfully added {len(chunks)} chunks to {collection_name} collection"}), 200
+        # Save images descriptions
+        descriptions = [image.description for image in images]
+        client.ingest_chunks(chunks=descriptions, collection_name="images")
+        logging.info(f"Successfully added {len(descriptions)} descriptions to 'images' collection")
 
+        return jsonify({"message": f"Successfully added documents ({len(content_chunks)}) and images description ({len(descriptions)})"}), 201
     except Exception as e:
         logging.error(f"Something went wrong: {e}")
-        return jsonify({"error": "Something went wrong", "detail": e}), 500
+        return jsonify({"error": "Something went wrong", "detail": str(e)}), 500
